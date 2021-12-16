b4d328b7eb7e43163ba781b2220058f6653a5ef3
Roger Huba <rogerhuba@gmail.com>
2021-12-16 00:40:47 -0600

    update script

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 main(){
-    if code --version && git --version && node --version && npm --versioneslint --version && tree --version && cat ~/.gitconfig && >/dev/null ; then
+    if code --version >/dev/null && git --version >/dev/null && node --version >/dev/null && npm --version >/dev/null && eslint --version >/dev/null && tree --version >/dev/null && cat ~/.gitconfig >/dev/null ; then
         echo -e "${GREEN}By gum, you've done it!"
     else
         echo -e "${RED}Something went wrong!"
@@ -12,4 +12,4 @@ main(){
 
 main
 
-## curl -s https://raw.githubusercontent.com/RogerHuba/test_remote_bash/main/test.sh | bash
+## curl -s https://raw.githubusercontent.com/RogerHuba/bash_test/main/test.sh | bash
